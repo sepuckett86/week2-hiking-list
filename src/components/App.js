db6191a6f49a923db4145d7d45f8f018cb0e2da7
sepuckett86 <sepuckett86@gmail.com>
2019-05-31 10:40:07 -0700

    add more to Search component

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,27 @@
 import Component from './Component.js';
 import Header from './Header.js';
+import Search from './Search.js';
 
 class App extends Component {
     render() {
         const dom = this.renderDOM();
+        const main = dom.querySelector('main');
 
         const header = new Header();
         const headerDOM = header.render();
 
+        const search = new Search();
+        const searchDOM = search.render();
+
         dom.prepend(headerDOM);
-        
+        main.appendChild(searchDOM);
+
         return dom;
     }
     renderTemplate() {
         return /*html*/ `
             <div>
-                <main>App
+                <main>
                 </main>
             </div>
         `;
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,26 @@
 import Component from './Component.js';
 
 class Search extends Component {
+    render() {
+        const dom = this.renderDOM();
+
+        const form = dom.querySelector('form');
+        const input = dom.querySelector('input');
+
+        form.addEventListener('submit', (event) => {
+            event.preventDefault();
+            console.log(input.value);
+        });
+        
+        return dom;
+    }
     renderTemplate() {
         return /*html*/ `
             <section>
-                Search for hikes near Portland!
+                Search hikes within 1 mile of Portland:
                 <form>
-                    <select>
-                        <option></option>
-                    </select>
+                    <input type="text">
+                    <button>Submit</button>
                 </form>
             </section>
         `;
